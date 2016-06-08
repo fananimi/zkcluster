75322fc24a5ffdfc35423d5a05a365b49984f354
Fanani M. Ihsan <fanani.mi@gmail.com>
2016-06-08 23:19:49 +0700

    update template

--- zkcluster/urls.py
+++ zkcluster/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^terminal/$', views.terminal, name='terminal')
 ]
--- zkcluster/views.py
+++ zkcluster/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'zkcluster/index.html')
+
+def dashboard(request):
+    return render(request, 'zkcluster/dashboard.html')
+
+def terminal(request):
+    return render(request, 'zkcluster/dashboard.html')
