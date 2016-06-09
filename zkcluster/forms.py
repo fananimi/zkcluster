562d421fda792e804c7935447578efdf1b2ddf02
Fanani M. Ihsan <fanani.mi@gmail.com>
2016-06-09 15:10:58 +0700

    add form to register terminal

--- zkcluster/forms.py
+++ zkcluster/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.utils.translation import ugettext as _
 
+from .models import Terminal
+
 
 class ScanTerminal(forms.Form):
     ip = forms.CharField(
@@ -19,20 +21,57 @@ class ScanTerminal(forms.Form):
         })
     )
 
-class AddTerminal(ScanTerminal):
+    def clean_ip(self):
+        ip = self.cleaned_data.get('ip')
+        terminal = Terminal.objects.filter(ip__iexact=ip).first()
+        if terminal:
+            raise forms.ValidationError(_('IP already exists'))
+        return ip
+
+class SaveTerminal(forms.ModelForm, ScanTerminal):
     serialnumber = forms.CharField(
         label=_('Serial Number'),
+        required=False,
         max_length=100,
         widget=forms.TextInput(attrs={
             'placeholder': _('Serial number'),
             'class': 'form-control',
+            'readonly': True
         })
     )
     name = forms.CharField(
         label=_('Name'),
+        required=False,
         max_length=200,
         widget=forms.TextInput(attrs={
             'placeholder': _('Terminal name'),
             'class': 'form-control',
         })
     )
+
+    def __init__(self, *args, **kwargs):
+        super(SaveTerminal, self).__init__(*args, **kwargs)
+        self.validate_name = False
+        if len(args) > 1:
+            self.validate_name = args[1].get('validate_name')
+
+        self.fields['ip'].widget = forms.TextInput(attrs={
+            'placeholder': _('IP Address'),
+            'class': 'form-control',
+            'readonly': True
+        })
+        self.fields['port'].widget = forms.TextInput(attrs={
+            'class': 'form-control',
+            'readonly': True
+        })
+
+    def clean_serialnumber(self):
+        serialnumber = self.cleaned_data.get('serialnumber')
+        terminal = Terminal.objects.filter(serialnumber__iexact=serialnumber).first()
+        if terminal:
+            raise forms.ValidationError(_('Serial number already exists'))
+        return serialnumber
+
+    class Meta:
+        model = Terminal
+        fields = ('ip', 'port', 'serialnumber', 'name')
--- zkcluster/views.py
+++ zkcluster/views.py
@@ -1,7 +1,11 @@
+import zk
+import urlparse
+from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods as alowed
 
 from .models import Terminal
-from .forms import ScanTerminal, AddTerminal
+from .forms import ScanTerminal, SaveTerminal
 
 def index(request):
     return render(request, 'zkcluster/index.html')
@@ -16,19 +20,57 @@ def terminal(request):
     }
     return render(request, 'zkcluster/terminal.html', data)
 
-def terminal_scan(request):
+@alowed(['POST'])
+def terminal_save(request):
+    connected = request.GET.get('connected')
+    if connected:
+        form = SaveTerminal(request.POST or None, {'validate_name': True})
+        if form.is_valid():
+            form.save()
+            return redirect('zkcluster:terminal')
+    else:
+        form = SaveTerminal(request.POST or None)
+
+    data = {
+        'form': form
+    }
+    return render(request, 'zkcluster/terminal_save.html', data)
+
+@alowed(['GET', 'POST'])
+def terminal_add(request):
     form = ScanTerminal(request.POST or None)
     if request.POST and form.is_valid():
-        return redirect('zkcluster:terminal_add')
+        ip = form.cleaned_data['ip']
+        port = form.cleaned_data['port']
+
+        # connect to terminal
+        terminal = zk.ZK(ip, port, 5)
+        conn = False
+        try:
+            conn = terminal.connect()
+            if conn:
+                terminal.disable_device()
+                terminal.test_voice()
+                sn = terminal.get_serialnumber()
+
+                # manipulate the POST information
+                mutable = request.POST._mutable
+                request.POST._mutable = True
+                request.POST['serialnumber'] = sn
+                request.POST._mutable = mutable
+
+                terminal.enable_device()
+                return terminal_save(request)
+            else:
+                messages.add_message(request, messages.ERROR, 'can\'t connect to terminal')
+        except Exception, e:
+            messages.add_message(request, messages.ERROR, str(e))
+        finally:
+            if conn:
+                terminal.disconnect()
+
     data = {
         'form': form
     }
+
     return render(request, 'zkcluster/terminal_scan.html', data)
-
-
-def terminal_add(request):
-    form = AddTerminal(request.POST or None)
-    data = {
-        'form': form
-    }
-    return render(request, 'zkcluster/terminal_add.html', data)
