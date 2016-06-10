e32ab56e45d0f4b6170a82209fd662990ce8d874
Fanani M. Ihsan <fanani.mi@gmail.com>
2016-06-10 11:06:59 +0700

    update models

--- zkcluster/models.py
+++ zkcluster/models.py
@@ -14,35 +14,16 @@ class Terminal(models.Model):
     ip = models.CharField(_('ip'), max_length=15, unique=True)
     port = models.IntegerField(_('port'), default=4370)
 
-    def __unicode__(self):
-        return self.name
-
-class User(models.Model):
-    USER_DEFAULT        = 0
-    USER_ADMIN          = 14
-
-    PRIVILEGE_COICES = (
-        (USER_DEFAULT, _('User')),
-        (USER_ADMIN, _('Administrator'))
-    )
-
-    uid = models.IntegerField(_('uid'))
-    name = models.CharField(_('name'), max_length=28)
-    privilege = models.SmallIntegerField(_('privilege'), choices=PRIVILEGE_COICES, default=USER_DEFAULT)
-    password = models.CharField(_('password'), max_length=8, blank=True, null=True)
-    group_id = models.CharField(_('group id'), max_length=7, blank=True, null=True)
-    terminal = models.ForeignKey(Terminal, related_name='zkuser')
-
     def __init__(self, *arg, **kwargs):
         self.zkconn = None
-        super(User, self).__init__(*arg, **kwargs)
+        super(Terminal, self).__init__(*arg, **kwargs)
 
     def __unicode__(self):
         return self.name
 
     def zk_connect(self):
-        ip = self.terminal.ip
-        port = self.terminal.port
+        ip = self.ip
+        port = self.port
         terminal = zk.ZK(ip, port, 5)
         try:
             conn = terminal.connect()
@@ -65,17 +46,36 @@ class User(models.Model):
             raise ZKError(_('terminal connection error'))
         self.zkconn.test_voice()
 
-    def zk_setuser(self):
+    def zk_setuser(self, uid, name, privilege, password, user_id):
         if not self.zkconn:
             raise ZKError(_('terminal connection error'))
 
         self.zkconn.set_user(
-            uid=int(self.id),
-            name=str(self.name),
-            privilege=int(self.privilege),
-            password=str(self.password),
-            user_id=str(self.id)
+            uid=int(uid),
+            name=str(name),
+            privilege=int(privilege),
+            password=str(password),
+            user_id=str(user_id)
         )
+
+class User(models.Model):
+    USER_DEFAULT        = 0
+    USER_ADMIN          = 14
+
+    PRIVILEGE_COICES = (
+        (USER_DEFAULT, _('User')),
+        (USER_ADMIN, _('Administrator'))
+    )
+
+    uid = models.IntegerField(_('uid'))
+    name = models.CharField(_('name'), max_length=28)
+    privilege = models.SmallIntegerField(_('privilege'), choices=PRIVILEGE_COICES, default=USER_DEFAULT)
+    password = models.CharField(_('password'), max_length=8, blank=True, null=True)
+    group_id = models.CharField(_('group id'), max_length=7, blank=True, null=True)
+    terminal = models.ForeignKey(Terminal, related_name='zkuser')
+
+    def __unicode__(self):
+        return self.name
 
     def save(self, *args, **kwargs):
         try:
@@ -85,12 +85,18 @@ class User(models.Model):
             uid = 1
 
         self.uid = uid
-        self.zk_connect()
+        self.terminal.zk_connect()
         super(User, self).save(*args, **kwargs)
 
 @receiver(post_save, sender=User)
 def on_save_user(sender, **kwargs):
     instance = kwargs['instance']
-    instance.zk_setuser()
-    instance.zk_voice()
-    instance.zk_disconnect()
+    instance.terminal.zk_setuser(
+        instance.id,
+        instance.name,
+        instance.privilege,
+        instance.password,
+        instance.uid
+    )
+    instance.terminal.zk_voice()
+    instance.terminal.zk_disconnect()
