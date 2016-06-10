b4f349bb548957f784bec5f4df43f6187fc27f95
Fanani M. Ihsan <fanani.mi@gmail.com>
2016-06-10 10:57:08 +0700

    simplicity zk connection in models

--- zkcluster/models.py
+++ zkcluster/models.py
@@ -33,8 +33,49 @@ class User(models.Model):
     group_id = models.CharField(_('group id'), max_length=7, blank=True, null=True)
     terminal = models.ForeignKey(Terminal, related_name='zkuser')
 
+    def __init__(self, *arg, **kwargs):
+        self.zkconn = None
+        super(User, self).__init__(*arg, **kwargs)
+
     def __unicode__(self):
         return self.name
+
+    def zk_connect(self):
+        ip = self.terminal.ip
+        port = self.terminal.port
+        terminal = zk.ZK(ip, port, 5)
+        try:
+            conn = terminal.connect()
+            if conn:
+                terminal.disable_device()
+                self.zkconn = terminal
+            else:
+                raise ZKError(_('can\'t connect to terminal'))
+        except Exception, e:
+            raise ZKError(str(e))
+
+    def zk_disconnect(self):
+        if not self.zkconn:
+            raise ZKError(_('terminal connection error'))
+        self.zkconn.enable_device()
+        self.zkconn.disconnect()
+
+    def zk_voice(self):
+        if not self.zkconn:
+            raise ZKError(_('terminal connection error'))
+        self.zkconn.test_voice()
+
+    def zk_setuser(self):
+        if not self.zkconn:
+            raise ZKError(_('terminal connection error'))
+
+        self.zkconn.set_user(
+            uid=int(self.id),
+            name=str(self.name),
+            privilege=int(self.privilege),
+            password=str(self.password),
+            user_id=str(self.id)
+        )
 
     def save(self, *args, **kwargs):
         try:
@@ -44,36 +85,12 @@ class User(models.Model):
             uid = 1
 
         self.uid = uid
-
-        # connect to terminal
-        ip = self.terminal.ip
-        port = self.terminal.port
-        terminal = zk.ZK(ip, port, 5)
-        conn = False
-        try:
-            conn = terminal.connect()
-            if conn:
-                setattr(self, 'connection', terminal)
-                terminal.disable_device()
-            else:
-                err_msg = _('can\'t connect to terminal')
-                raise ZKError(err_msg)
-        except Exception, e:
-            raise ZKError(str(e))
-
+        self.zk_connect()
         super(User, self).save(*args, **kwargs)
 
 @receiver(post_save, sender=User)
 def on_save_user(sender, **kwargs):
     instance = kwargs['instance']
-    conn = instance.connection
-    conn.set_user(
-        uid=int(instance.id),
-        name=str(instance.name),
-        privilege=int(instance.privilege),
-        password=str(instance.password),
-        user_id=str(instance.id)
-    )
-    conn.test_voice()
-    conn.enable_device()
-    conn.disconnect()
+    instance.zk_setuser()
+    instance.zk_voice()
+    instance.zk_disconnect()
